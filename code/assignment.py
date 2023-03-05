1371f36feca91a3915bd37d11d222b346aac7c1c
mikaylawalsh <mikayla_walsh@brown.edu>
2023-03-05 10:05:06 -0500

    wip

--- code/assignment.py
+++ code/assignment.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from re import L
 from matplotlib import pyplot as plt
 from preprocess import get_data
 from convolution import conv2d
@@ -12,6 +13,7 @@ import math
 # ensures that we run only on cpu
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
+
 class Model(tf.keras.Model):
     def __init__(self):
         """
@@ -23,16 +25,36 @@ class Model(tf.keras.Model):
 
         self.batch_size = None
         self.num_classes = None
-        self.loss_list = [] # Append losses to this list in training so you can visualize loss vs time in main
+        # Append losses to this list in training so you can visualize loss vs time in main
+        self.loss_list = []
 
         # TODO: Initialize all hyperparameters
 
+        # learning rate, kernel size, stride, batch size, epochs, padding, size of hidden layers
+        # optimizer (adam), output dimensions
+        self.learning_rate = 0.01
+        self.epochs = 20
+        self.stride = 1
+        self.optimizer = tf.keras.optimizers.Adam()
+        self.padding = "SAME"  # valid or same but thats not an option
+
         # TODO: Initialize all trainable parameters
+        self.weights = tf.Variable(tf.random.truncated_normal(
+            [100, self.num_classes], stddev=.1))  # shape right?
+        self.biases = tf.Variable(
+            tf.random.truncated_normal([self.num_classes], stddev=.1))
+        # how many filters should i have? what size should they be?
+        self.filters = tf.Variable(
+            tf.random.truncated_normal([5, 5, 3, 16], stddev=.1))
+
+        self.flatten = tf.keras.layers.Flatten()
+
+        # weights, biases, kernel values
 
     def call(self, inputs, is_testing=False):
         """
         Runs a forward pass on an input batch of images.
-        
+
         :param inputs: images, shape of (num_inputs, 32, 32, 3); during training, the shape is (batch_size, 32, 32, 3)
         :param is_testing: a boolean that should be set to True only when you're doing Part 2 of the assignment and this function is being called during testing
         :return: logits - a matrix of shape (num_inputs, num_classes); during training, it would be (batch_size, 2)
@@ -42,36 +64,58 @@ class Model(tf.keras.Model):
         # shape of filter = (filter_height, filter_width, in_channels, out_channels)
         # shape of strides = (batch_stride, height_stride, width_stride, channels_stride)
 
-        pass
+        # add more layers later..basic model
+
+        if is_testing:
+            lay1 = conv2d(inputs, self.filters, self.stride, self.padding)
+        else:
+            # should stride be [1, 1, 1, 1] or 2? conflicting
+            lay1 = tf.nn.conv2d(inputs, self.filters, 2, "SAME")
+
+        # what goes here for value? correct addition?
+        lay1 += tf.nn.bias_add(lay1, self.biases)
+        mean, variance = tf.nn.moments(inputs, [0, 1, 2])  # ??
+        lay1 = tf.nn.batch_normalization(
+            lay1, mean, variance, None, None, 1e-5)  # what goes as input
+        lay2 = tf.nn.relu(lay1)  # ?
+        lay3 = tf.nn.max_pool(lay2, 3, 2, "SAME")
+        logits = tf.matmul(lay3, self.weights) + self.biases
+
+        return logits
+        # is output shape correct?
 
     def loss(self, logits, labels):
         """
         Calculates the model cross-entropy loss after one forward pass.
         Softmax is applied in this function.
-        
+
         :param logits: during training, a matrix of shape (batch_size, self.num_classes) 
         containing the result of multiple convolution and feed forward layers
         :param labels: during training, matrix of shape (batch_size, self.num_classes) containing the train labels
         :return: the loss of the model as a Tensor
         """
 
-        pass
+        probs = tf.nn.softmax_cross_entropy_with_logits(labels, logits)
+
+        return probs
 
     def accuracy(self, logits, labels):
         """
         Calculates the model's prediction accuracy by comparing
         logits to correct labels – no need to modify this.
-        
+
         :param logits: a matrix of size (num_inputs, self.num_classes); during training, this will be (batch_size, self.num_classes)
         containing the result of multiple convolution and feed forward layers
         :param labels: matrix of size (num_labels, self.num_classes) containing the answers, during training, this will be (batch_size, self.num_classes)
 
         NOTE: DO NOT EDIT
-        
+
         :return: the accuracy of the model as a Tensor
         """
-        correct_predictions = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
+        correct_predictions = tf.equal(
+            tf.argmax(logits, 1), tf.argmax(labels, 1))
         return tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+
 
 def train(model, train_inputs, train_labels):
     '''
@@ -79,7 +123,7 @@ def train(model, train_inputs, train_labels):
     and labels - ensure that they are shuffled in the same order using tf.gather or zipping.
     To increase accuracy, you may want to use tf.image.random_flip_left_right on your
     inputs before doing the forward pass. You should batch your inputs.
-    
+
     :param model: the initialized model to use for the forward pass and backward pass
     :param train_inputs: train inputs (all inputs to use for training), 
     shape (num_inputs, width, height, num_channels)
@@ -88,13 +132,44 @@ def train(model, train_inputs, train_labels):
     :return: Optionally list of losses per batch to use for visualize_loss
     '''
 
-    pass
+    # 1. shuffle inputs (tf.random.shuffle and tf.gather) -- i don't understand how zip would work here...
+
+    # indices = []
+    # for x in range(len(train_inputs)):
+    #     indices.append(x)
+    indices = [x for x in range(len(train_inputs))]
+    indices = tf.random.shuffle(indices)
+    shuffled_inputs = tf.gather(train_inputs, indices)
+    shuffled_labels = tf.gather(train_labels, indices)
+
+    # 2. make sure inputs are of correct size - (batch_size, width, height, in_channels)
+    # assert(shuffled_inputs.shape == (self.batch_size, ))
+
+    # 3. call tf.image.random_flip_left_right to increase accuracy -- call this on all?
+    # shuffled_inputs = tf.image.random_flip_left_right(shuffled_inputs)
+
+    # 4. call call
+    # 5. calculate loss within scope of tf.GradientTape
+    with tf.GradientTape as tape:
+        logits = model.call(shuffled_inputs, False)
+        loss = model.loss(logits, shuffled_labels)
+        model.loss_list.append(loss)
+
+    # 6. use optimizer to apply gradients outside of gradient tape
+    grads = tape.gradient(loss, model.weights)
+    model.optimizer.apply_gradients(model.weights, grads)
+    # 7. calculate accuracy
+    acc = model.accuracy(logits, shuffled_labels)
+
+    # 8. print list of losses per batch
+    return {'loss': loss, 'acc': acc}
+
 
 def test(model, test_inputs, test_labels):
     """
     Tests the model on the test inputs and labels. You should NOT randomly 
     flip images or do any extra preprocessing.
-    
+
     :param test_inputs: test data (all images to be tested), 
     shape (num_inputs, width, height, num_channels)
     :param test_labels: test labels (all corresponding labels),
@@ -102,11 +177,15 @@ def test(model, test_inputs, test_labels):
     :return: test accuracy - this should be the average accuracy across
     all batches
     """
+    logits = model.call(test_inputs, False)
+    loss = model.loss(logits, test_labels)
 
-    pass
+    acc = model.accuracy(logits, test_labels)
+
+    return {'loss': loss, 'acc': acc}
 
 
-def visualize_loss(losses): 
+def visualize_loss(losses):
     """
     Uses Matplotlib to visualize the losses of our model.
     :param losses: list of loss data stored from train. Can use the model's loss_list 
@@ -121,7 +200,7 @@ def visualize_loss(losses):
     plt.title('Loss per batch')
     plt.xlabel('Batch')
     plt.ylabel('Loss')
-    plt.show()  
+    plt.show()
 
 
 def visualize_results(image_inputs, probabilities, image_labels, first_label, second_label):
@@ -139,11 +218,12 @@ def visualize_results(image_inputs, probabilities, image_labels, first_label, se
     one for incorrect results
     """
     # Helper function to plot images into 10 columns
-    def plotter(image_indices, label): 
+    def plotter(image_indices, label):
         nc = 10
         nr = math.ceil(len(image_indices) / 10)
         fig = plt.figure()
-        fig.suptitle("{} Examples\nPL = Predicted Label\nAL = Actual Label".format(label))
+        fig.suptitle(
+            "{} Examples\nPL = Predicted Label\nAL = Actual Label".format(label))
         for i in range(len(image_indices)):
             ind = image_indices[i]
             ax = fig.add_subplot(nr, nc, i+1)
@@ -155,17 +235,17 @@ def visualize_results(image_inputs, probabilities, image_labels, first_label, se
             plt.setp(ax.get_xticklabels(), visible=False)
             plt.setp(ax.get_yticklabels(), visible=False)
             ax.tick_params(axis='both', which='both', length=0)
-        
+
     predicted_labels = np.argmax(probabilities, axis=1)
     num_images = image_inputs.shape[0]
 
     # Separate correct and incorrect images
     correct = []
     incorrect = []
-    for i in range(num_images): 
-        if predicted_labels[i] == np.argmax(image_labels[i], axis=0): 
+    for i in range(num_images):
+        if predicted_labels[i] == np.argmax(image_labels[i], axis=0):
             correct.append(i)
-        else: 
+        else:
             incorrect.append(i)
 
     plotter(correct, 'Correct')
@@ -178,15 +258,29 @@ def main():
     Read in CIFAR10 data (limited to 2 classes), initialize your model, and train and 
     test your model for a number of epochs. We recommend that you train for
     10 epochs and at most 25 epochs. 
-    
+
     CS1470 students should receive a final accuracy 
     on the testing examples for cat and dog of >=70%.
-    
+
     CS2470 students should receive a final accuracy 
     on the testing examples for cat and dog of >=75%.
-    
+
     :return: None
     '''
+
+    # 1. get train and test data
+    train_inputs, train_labels = get_data(
+        "/Users/mikaylawalsh/Desktop/deep_learning/hw3-mikaylawalsh/data/train", 3, 5)
+    test_inputs, test_labels = get_data(
+        "/Users/mikaylawalsh/Desktop/deep_learning/hw3-mikaylawalsh/data/test", 3, 5)
+
+    # 2. initialize model
+    model = Model()
+    # 3. train it for many epochs (10) - use for loop
+    for x in range(model.epochs):
+        train(model, train_inputs, train_labels)
+    # 4. call test method
+    test(model, test_inputs, test_labels)
 
     return
 
--- code/convolution.py
+++ code/convolution.py
@@ -6,107 +6,155 @@ import numpy as np
 import random
 import math
 
+
 def conv2d(inputs, filters, strides, padding):
-	"""
-	Performs 2D convolution given 4D inputs and filter Tensors.
-	:param inputs: tensor with shape [num_examples, in_height, in_width, in_channels]
-	:param filters: tensor with shape [filter_height, filter_width, in_channels, out_channels]
-	:param strides: MUST BE [1, 1, 1, 1] - list of strides, with each stride corresponding to each dimension in input
-	:param padding: either "SAME" or "VALID", capitalization matters
-	:return: outputs, Tensor with shape [num_examples, output_height, output_width, output_channels]
-	"""
-	num_examples = None
-	in_height = None
-	in_width = None
-	input_in_channels = None
+    """
+    Performs 2D convolution given 4D inputs and filter Tensors.
+    :param inputs: tensor with shape [num_examples, in_height, in_width, in_channels]
+    :param filters: tensor with shape [filter_height, filter_width, in_channels, out_channels]
+    :param strides: MUST BE [1, 1, 1, 1] - list of strides, with each stride corresponding to each dimension in input
+    :param padding: either "SAME" or "VALID", capitalization matters
+    :return: outputs, Tensor with shape [num_examples, output_height, output_width, output_channels]
+    """
 
-	filter_height = None
-	filter_width = None
-	filter_in_channels = None
-	filter_out_channels = None
+    # how to i get these values
+    num_examples = None
+    in_height = None
+    in_width = None
+    input_in_channels = None
 
-	num_examples_stride = None
-	strideY = None
-	strideX = None
-	channels_stride = None
+    filter_height = None
+    filter_width = None
+    filter_in_channels = None
+    filter_out_channels = None
 
-	# Cleaning padding input
+    num_examples_stride = None
+    strideY = None
+    strideX = None
+    channels_stride = None
 
-	# Calculate output dimensions
+    # 1. assert inputs "in channels" is equal to filter's "in channels"
+    assert(input_in_channels == filter_in_channels)
+    # 2. check if padding is SAME or VALID
+    # 3. if padding is SAME, calculate how much you need with (filter_size - 1)/2 and round using math.floor
+    if padding == "SAME":
+        pad_height = math.floor(filter_height - 1)/2
+        pad_width = math.floor(filter_width - 1)/2
+    else:
+        pad_height = 0
+        pad_width = 0
+    # 4. use np.pad to pad input
+    padded_input = np.pad(inputs, pad_width)
+    # 5. create a NumPy array with the correct output dimensions (below)
+    output_height = (in_height + 2*pad_height - filter_height) / strideY + 1
+    output_width = (in_width + 2*pad_width - filter_width) / strideX + 1
+    output = np.ones((output_height, output_width))
+    # 6. update each element in the output as you perform the convolution operator for each image
+    # 7. stop iterating when filter does not fit over rest of padding input
+    # 8. perform the convolution per input channel and sum those dot products together
+    while ...:
+        for m in range(filter_height):  # i think i use my strides here to skip by multiple
+            for n in range(filter_width):
+                output[m][n] = inputs[m][n] * filters[m][n]
+    # 9. return a tensor
+    return tf.convert_to_tensor(output, dtype=tf.float32)
+    # Cleaning padding input
 
-	# PLEASE RETURN A TENSOR. HINT: tf.convert_to_tensor(your_array, dtype = tf.float32)
+    # Calculate output dimensions
+    # height = (in_height + 2*padY - filter_height) / strideY + 1
+    # width = (in_width + 2*padX - filter_width) / strideX + 1
 
-	pass
+    # PLEASE RETURN A TENSOR. HINT: tf.convert_to_tensor(your_array, dtype = tf.float32)
+
 
 def same_test_0():
-	'''
-	Simple test using SAME padding to check out differences between 
-	own convolution function and TensorFlow's convolution function.
+    '''
+    Simple test using SAME padding to check out differences between 
+    own convolution function and TensorFlow's convolution function.
 
-	NOTE: DO NOT EDIT
-	'''
-	imgs = np.array([[2,2,3,3,3],[0,1,3,0,3],[2,3,0,1,3],[3,3,2,1,2],[3,3,0,2,3]], dtype=np.float32)
-	imgs = np.reshape(imgs, (1,5,5,1))
-	filters = tf.Variable(tf.random.truncated_normal([2, 2, 1, 1],
-								dtype=tf.float32,
-								stddev=1e-1),
-								name="filters")
-	my_conv = conv2d(imgs, filters, strides=[1, 1, 1, 1], padding="SAME")
-	tf_conv = tf.nn.conv2d(imgs, filters, [1, 1, 1, 1], padding="SAME")
-	print("SAME_TEST_0:", "my conv2d:", my_conv[0][0][0], "tf conv2d:", tf_conv[0][0][0].numpy())
+    NOTE: DO NOT EDIT
+    '''
+    imgs = np.array([[2, 2, 3, 3, 3], [0, 1, 3, 0, 3], [2, 3, 0, 1, 3], [
+                    3, 3, 2, 1, 2], [3, 3, 0, 2, 3]], dtype=np.float32)
+    imgs = np.reshape(imgs, (1, 5, 5, 1))
+    filters = tf.Variable(tf.random.truncated_normal([2, 2, 1, 1],
+                                                     dtype=tf.float32,
+                                                     stddev=1e-1),
+                          name="filters")
+    my_conv = conv2d(imgs, filters, strides=[1, 1, 1, 1], padding="SAME")
+    tf_conv = tf.nn.conv2d(imgs, filters, [1, 1, 1, 1], padding="SAME")
+    print("SAME_TEST_0:", "my conv2d:",
+          my_conv[0][0][0], "tf conv2d:", tf_conv[0][0][0].numpy())
+
 
 def valid_test_0():
-	'''
-	Simple test using VALID padding to check out differences between 
-	own convolution function and TensorFlow's convolution function.
+    '''
+    Simple test using VALID padding to check out differences between 
+    own convolution function and TensorFlow's convolution function.
 
-	NOTE: DO NOT EDIT
-	'''
-	imgs = np.array([[2,2,3,3,3],[0,1,3,0,3],[2,3,0,1,3],[3,3,2,1,2],[3,3,0,2,3]], dtype=np.float32)
-	imgs = np.reshape(imgs, (1,5,5,1))
-	filters = tf.Variable(tf.random.truncated_normal([2, 2, 1, 1],
-								dtype=tf.float32,
-								stddev=1e-1),
-								name="filters")
-	my_conv = conv2d(imgs, filters, strides=[1, 1, 1, 1], padding="VALID")
-	tf_conv = tf.nn.conv2d(imgs, filters, [1, 1, 1, 1], padding="VALID")
-	print("VALID_TEST_0:", "my conv2d:", my_conv[0][0], "tf conv2d:", tf_conv[0][0].numpy())
+    NOTE: DO NOT EDIT
+    '''
+    imgs = np.array([[2, 2, 3, 3, 3], [0, 1, 3, 0, 3], [2, 3, 0, 1, 3], [
+                    3, 3, 2, 1, 2], [3, 3, 0, 2, 3]], dtype=np.float32)
+    imgs = np.reshape(imgs, (1, 5, 5, 1))
+    filters = tf.Variable(tf.random.truncated_normal([2, 2, 1, 1],
+                                                     dtype=tf.float32,
+                                                     stddev=1e-1),
+                          name="filters")
+    my_conv = conv2d(imgs, filters, strides=[1, 1, 1, 1], padding="VALID")
+    tf_conv = tf.nn.conv2d(imgs, filters, [1, 1, 1, 1], padding="VALID")
+    print("VALID_TEST_0:", "my conv2d:",
+          my_conv[0][0], "tf conv2d:", tf_conv[0][0].numpy())
+
 
 def valid_test_1():
-	'''
-	Simple test using VALID padding to check out differences between 
-	own convolution function and TensorFlow's convolution function.
+    '''
+    Simple test using VALID padding to check out differences between 
+    own convolution function and TensorFlow's convolution function.
 
-	NOTE: DO NOT EDIT
-	'''
-	imgs = np.array([[3,5,3,3],[5,1,4,5],[2,5,0,1],[3,3,2,1]], dtype=np.float32)
-	imgs = np.reshape(imgs, (1,4,4,1))
-	filters = tf.Variable(tf.random.truncated_normal([3, 3, 1, 1],
-								dtype=tf.float32,
-								stddev=1e-1),
-								name="filters")
-	my_conv = conv2d(imgs, filters, strides=[1, 1, 1, 1], padding="VALID")
-	tf_conv = tf.nn.conv2d(imgs, filters, [1, 1, 1, 1], padding="VALID")
-	print("VALID_TEST_1:", "my conv2d:", my_conv[0][0], "tf conv2d:", tf_conv[0][0].numpy())
+    NOTE: DO NOT EDIT
+    '''
+    imgs = np.array([[3, 5, 3, 3], [5, 1, 4, 5], [2, 5, 0, 1],
+                    [3, 3, 2, 1]], dtype=np.float32)
+    imgs = np.reshape(imgs, (1, 4, 4, 1))
+    filters = tf.Variable(tf.random.truncated_normal([3, 3, 1, 1],
+                                                     dtype=tf.float32,
+                                                     stddev=1e-1),
+                          name="filters")
+    my_conv = conv2d(imgs, filters, strides=[1, 1, 1, 1], padding="VALID")
+    tf_conv = tf.nn.conv2d(imgs, filters, [1, 1, 1, 1], padding="VALID")
+    print("VALID_TEST_1:", "my conv2d:",
+          my_conv[0][0], "tf conv2d:", tf_conv[0][0].numpy())
+
 
 def valid_test_2():
-	'''
-	Simple test using VALID padding to check out differences between 
-	own convolution function and TensorFlow's convolution function.
+    '''
+    Simple test using VALID padding to check out differences between 
+    own convolution function and TensorFlow's convolution function.
 
-	NOTE: DO NOT EDIT
-	'''
-	imgs = np.array([[1,3,2,1],[1,3,3,1],[2,1,1,3],[3,2,3,3]], dtype=np.float32)
-	imgs = np.reshape(imgs, (1,4,4,1))
-	filters = np.array([[1,2,3],[0,1,0],[2,1,2]]).reshape((3,3,1,1)).astype(np.float32)
-	my_conv = conv2d(imgs, filters, strides=[1, 1, 1, 1], padding="VALID")
-	tf_conv = tf.nn.conv2d(imgs, filters, [1, 1, 1, 1], padding="VALID")
-	print("VALID_TEST_1:", "my conv2d:", my_conv[0][0], "tf conv2d:", tf_conv[0][0].numpy())
+    NOTE: DO NOT EDIT
+    '''
+    imgs = np.array([[1, 3, 2, 1], [1, 3, 3, 1], [2, 1, 1, 3],
+                    [3, 2, 3, 3]], dtype=np.float32)
+    imgs = np.reshape(imgs, (1, 4, 4, 1))
+    filters = np.array([[1, 2, 3], [0, 1, 0], [2, 1, 2]]).reshape(
+        (3, 3, 1, 1)).astype(np.float32)
+    my_conv = conv2d(imgs, filters, strides=[1, 1, 1, 1], padding="VALID")
+    tf_conv = tf.nn.conv2d(imgs, filters, [1, 1, 1, 1], padding="VALID")
+    print("VALID_TEST_1:", "my conv2d:",
+          my_conv[0][0], "tf conv2d:", tf_conv[0][0].numpy())
+
 
 def main():
-	# TODO: Add in any tests you may want to use to view the differences between your and TensorFlow's output
+    # TODO: Add in any tests you may want to use to view the differences between your and TensorFlow's output
 
-	return
+    # 1. test to make sure results are very similar
+    same_test_0()
+    valid_test_0()
+    valid_test_1()
+    valid_test_2()
+    return
+
 
 if __name__ == '__main__':
-	main()
+    main()
